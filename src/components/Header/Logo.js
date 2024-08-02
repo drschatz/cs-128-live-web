894236246f0f0ad1b318c9cbc94bce77b332f1cd
Jule Schatz <drschatz@illinois.edu>
2024-08-02 14:28:42 -0500

    logo change

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -5,15 +5,6 @@ import nextIMG from "public/classLogo.jpg";
 const Logo = () => {
   return (
     <Link href="/" className="flex items-center text-dark dark:text-light">
-      {/* <div className=" w-12 md:w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4">
-        <Image
-          src={nextIMG}
-          alt="CS128-LIVE logo"
-          className="w-full h-auto rounded-full"
-          sizes="20vw"
-          priority
-        />
-      </div> */}
       <span className="font-bold dark:font-semibold text-lg md:text-xl hover:text-accent">
         /* CS128 LIVE */
       </span>
@@ -22,3 +13,13 @@ const Logo = () => {
 };
 
 export default Logo;
+
+{/* <div className=" w-12 md:w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4">
+<Image
+  src={nextIMG}
+  alt="CS128-LIVE logo"
+  className="w-full h-auto rounded-full"
+  sizes="20vw"
+  priority
+/>
+</div> */}
