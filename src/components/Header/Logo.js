71ff03751b07a61c7e7251628b60c3c8cadf7b20
Jule Schatz <drschatz@illinois.edu>
2024-08-02 14:22:23 -0500

    small syllabus changes and logo attempt

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
-import nextIMG from "public/next.svg";
+import nextIMG from "public/classLogo.jpg";
 
 const Logo = () => {
   return (
     <Link href="/" className="flex items-center text-dark dark:text-light">
       <div className=" w-12 md:w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4">
         <Image
-          src="public/profile_img/squ.jpg"
+          src={nextIMG}
           alt="CS128-LIVE logo"
           className="w-full h-auto rounded-full"
           sizes="20vw"
