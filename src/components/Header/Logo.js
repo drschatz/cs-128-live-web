35b155d559550e60d722525bdc475b97e960e138
Jule Schatz <drschatz@illinois.edu>
2024-08-02 14:38:13 -0500

    think i figured out the issue

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
-import nextIMG from "public/classLogo.jpg";
+import nextIMG from "public/classLogo.png";
 
 const Logo = () => {
   return (
     <Link href="/" className="flex items-center text-dark dark:text-light">
+      <div className=" w-12 md:w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4">
         <Image
           src={nextIMG}
           alt="CS128-LIVE logo"
@@ -12,8 +13,9 @@ const Logo = () => {
           sizes="20vw"
           priority
         />
+      </div>
       <span className="font-bold dark:font-semibold text-lg md:text-xl hover:text-accent">
-        /* CS128 LIVE */
+      &#47;&#42;CS128 LIVE &#42;&#47;
       </span>
     </Link>
   );
