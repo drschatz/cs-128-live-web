8e40d6b4db447f08565188e21c1936310db687fc
Lumi Xu <102558017+Lumi-works@users.noreply.github.com>
2024-08-08 23:56:17 +0800

    nav bar fix

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -18,60 +18,10 @@ const Header = () => {
       <div className="flex items-center justify-between font-semibold">
         <Logo />
         {/* nav links */}
-        <div className="hidden md:flex items-center space-x-4">
+        <div className="hidden sm:flex items-center space-x-4">
           <Link href="/lectures" className="hover:text-accent">
             {" "}
-            Schedule{" "}
-          </Link>
-          {/* <Link href="/assignments" className="hover:text-accent">
-            {" "}
-            Assignments{" "}
-          </Link> */}
-          {/* <Link href="/quizzes" className="hover:text-accent">
-            {" "}
-            Quizzes{" "}
-          </Link> */}
-          <Link href="/syllabus" className="hover:text-accent">
-            {" "}
-            Syllabus{" "}
-          </Link>
-          {/* <Link href="/course-book" className="hover:text-accent">
-            {" "}
-            Course Book{" "}
-          </Link> */}
-          {/* <Link href="/support" className="hover:text-accent">
-            {" "}
-            Support{" "}
-          </Link> */}
-        </div>
-
-        {/* Hamburger Icon */}
-        <button
-          id="burger-btn"
-          className={
-            isOpen ? "open" : "block hamburger md:hidden focus:outline-none"
-          }
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          <span className="hamburger-top"></span>
-          <span className="hamburger-middle"></span>
-          <span className="hamburger-bottom"></span>
-        </button>
-      </div>
-
-      {/* Mobile Nav */}
-      <div className="md:hidden w-full">
-        <div
-          id="burger-menu"
-          className={
-            isOpen
-              ? "flex absolute flex-col items-center self-end py-8 space-y-6 bg-white sm:w-auto sm:self-center left-0 right-0 mt-2 drop-shadow-md"
-              : "hidden"
-          }
-        >
-          <Link href="/lectures" className="hover:text-accent">
-            {" "}
-            Schedule{" "}
+            Lectures{" "}
           </Link>
           <Link href="/assignments" className="hover:text-accent">
             {" "}
@@ -85,10 +35,74 @@ const Header = () => {
             {" "}
             Syllabus{" "}
           </Link>
-          <Link href="/course-book" className="hover:text-accent">
+          {/* <Link href="/course-book" className="hover:text-accent">
             {" "}
             Course Book{" "}
+          </Link> */}
+          <Link href="/support" className="hover:text-accent">
+            {" "}
+            Support{" "}
           </Link>
+        </div>
+
+        {/* Hamburger Icon */}
+        <button
+          id="burger-btn"
+          className={`flex sm:hidden focus:outline-none ${
+            isOpen ? "open" : ""
+          }`}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <div className="space-y-1">
+            <span
+              className={`block w-8 h-1 bg-black transform transition duration-300 ${
+                isOpen ? "rotate-45 translate-y-1.5" : ""
+              }`}
+            ></span>
+            <span
+              className={`block w-8 h-1 bg-black transition duration-300 ${
+                isOpen ? "opacity-0" : ""
+              }`}
+            ></span>
+            <span
+              className={`block w-8 h-1 bg-black transform transition duration-300 ${
+                isOpen ? "-rotate-45 -translate-y-2.5" : ""
+              }`}
+            ></span>
+          </div>
+        </button>
+      </div>
+
+      {/* Mobile Nav */}
+      <div className="sm:hidden w-full">
+        <div
+          id="burger-menu"
+          className={
+            isOpen
+              ? "flex absolute flex-col items-center self-end py-8 space-y-6 bg-white sm:w-auto sm:self-center left-0 right-0 mt-2 drop-shadow-md"
+              : "hidden"
+          }
+        >
+          <Link href="/lectures" className="hover:text-accent">
+            {" "}
+            Lectures{" "}
+          </Link>
+          {/* <Link href="/assignments" className="hover:text-accent">
+            {" "}
+            Assignments{" "}
+          </Link> */}
+          <Link href="/quizzes" className="hover:text-accent">
+            {" "}
+            Quizzes{" "}
+          </Link>
+          <Link href="/syllabus" className="hover:text-accent">
+            {" "}
+            Syllabus{" "}
+          </Link>
+          {/* <Link href="/course-book" className="hover:text-accent">
+            {" "}
+            Course Book{" "}
+          </Link> */}
           <Link href="/support" className="hover:text-accent">
             {" "}
             Support{" "}
