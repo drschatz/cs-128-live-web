e30b922318fa9f503115d416220c83c2f36ebd6c
Lumi Xu <102558017+Lumi-works@users.noreply.github.com>
2024-08-18 12:16:00 +0800

    change the People title to Staffs in staffs page

--- src/app/staff/page.js
+++ src/app/staff/page.js
@@ -6,7 +6,7 @@ const PeoplePage = () => {
       <div className="mb-8 text-center relative w-full h-[24vh] bg-pink-600 ">
         <div className="w-full z-10 flex flex-col items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           <h1 className="inline-block mt-6 font-semibold capitalize text-light text-2xl md:text-3xl lg:text-5xl !leading-normal relative w-5/6">
-            People
+            Staffs
           </h1>
         </div>
         <div className="absolute top-0 left-0 right-0 bottom-0 h-full bg-dark/60 dark:bg-dark/40" />
