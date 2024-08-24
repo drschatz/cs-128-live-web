d0cbfeba6591bf914d9ceec8341ee21b6b38eaf9
Lumi Xu <102558017+Lumi-works@users.noreply.github.com>
2024-08-24 16:59:29 -0500

    TOC for MPs but not for resource

--- src/app/course-book/[chapter]/[section]/page.js
+++ src/app/course-book/[chapter]/[section]/page.js
@@ -11,6 +11,8 @@ const DocPage = () => {
     (doc) => doc._raw.flattenedPath === `${params.chapter}/${params.section}`
   );
 
+  const chapterTitle = doc.chapterTitle;
+
   if (!doc) {
     return null;
   }
@@ -26,11 +28,55 @@ const DocPage = () => {
         <div className="absolute top-0 left-0 right-0 bottom-0 h-full bg-dark/60 dark:bg-dark/40" />
       </div>
 
-      <div className="flex justify-center mt-8 px-5 md:px-10">
-      <div className="w-full lg:w-8/12 pb-40">
-        <RenderMdx doc={doc} />
-      </div>
-    </div>
+      {chapterTitle === "MPs" ? (
+        <div className="grid grid-cols-12  gap-y-8 lg:gap-8 sxl:gap-16 mt-8 px-5 md:px-10">
+          {/* Table of Content */}
+          <div className="col-span-12  lg:col-span-4">
+            <details
+              className="border-[1px] border-solid border-dark dark:border-light text-dark dark:text-light rounded-lg p-4 sticky top-24 max-h-[80vh] overflow-hidden overflow-y-auto"
+              open
+            >
+              <summary className="text-lg font-semibold capitalize cursor-pointer">
+                Table Of Content
+              </summary>
+              <ul className="mt-4 font-in text-base">
+                {doc.toc.map((heading) => {
+                  return (
+                    <li key={`#${heading.slug}`} className="py-1">
+                      <a
+                        href={`#${heading.slug}`}
+                        data-level={heading.level}
+                        className="data-[level=two]:pl-0  data-[level=two]:pt-2
+                                     data-[level=two]:border-t border-solid border-dark/40
+                                     data-[level=three]:pl-4
+                                     sm:data-[level=three]:pl-6
+                                     flex items-center justify-start
+                                     "
+                      >
+                        {heading.level === "three" ? (
+                          <span className="flex w-1 h-1 rounded-full bg-dark mr-2">
+                            &nbsp;
+                          </span>
+                        ) : null}
+
+                        <span className="hover:underline">{heading.text}</span>
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </details>
+          </div>
+          {/* Main Content */}
+          <RenderMdx doc={doc} />
+        </div>
+      ) : (
+        <div className="flex justify-center mt-8 px-5 md:px-10">
+          <div className="w-full lg:w-8/12 pb-40">
+            <RenderMdx doc={doc} />
+          </div>
+        </div>
+      )}
     </article>
   );
 };
