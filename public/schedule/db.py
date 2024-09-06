e3b67022ab7a6d85b8f1f476520b524b15a52845
Lumi Xu <102558017+Lumi-works@users.noreply.github.com>
2024-09-06 11:40:50 -0500

    schedule page lab link

--- public/schedule/db.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-csv_file_path = 'public/schedules/calendar24.csv'
-df = pd.read_csv(csv_file_path)
-
-json_file_path = 'public/schedules/calendar24.json'
-df.to_json(json_file_path, orient='records', lines=True)
